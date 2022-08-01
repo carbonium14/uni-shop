d23e81772d545f93e668dfd91ac7387987af3818
carbonium14 <1075413520@qq.com>
2022-08-01 14:06:16 +0800

    finish index

--- main.js
+++ main.js
@@ -2,7 +2,24 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
-
+import {$http} from '@escook/request-miniprogram'
+uni.$http=$http
+$http.baseUrl="https://api-ugo-web.itheima.net"
+$http.beforeRequest=function(options) {
+	uni.showLoading({
+		title:'数据加载中...'
+	})
+}
+$http.afterRequest=function(options) {
+	uni.hideLoading()
+}
+uni.$showMsg=function(title="数据加载失败！",duration=1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'
