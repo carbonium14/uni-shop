4a62261f1f96d5a484336720ef3b62219f9a9909
carbonium14 <1075413520@qq.com>
2022-08-04 15:08:52 +0800

    finish settle

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -11,29 +11,95 @@
 		  </view>
 		
 		  <!-- 结算按钮 -->
-		  <view class="btn-settle">结算{{(checkedCount)}}</view>
+		  <view class="btn-settle" @click="settlement">结算{{(checkedCount)}}</view>
 	</view>
 </template>
 
 <script>
-	import {mapGetters,mapMutations} from 'vuex'
+	import {mapGetters,mapMutations,mapState} from 'vuex'
 	export default {
 		name:"my-settle",
 		data() {
 			return {
-				
+				second:3,
+				timer:null
 			};
 		},
 		computed: {
 			...mapGetters('m_cart', ['checkedCount','total','checkedGoodsAmount']),
+			...mapGetters('m_user', ['addstr']),
+			...mapState('m_user', ['token']),
+			...mapState('m_cart',['cart']),
 			isFullCheck() {
 				return this.total===this.checkedCount
 			}
 		},
 		methods: {
-			...mapMutations('m_cart', ['updateAllGoodsState']),
+			...mapMutations('m_cart', ['updateAllGoodsState','updateRedirectInfo']),
 			changeAllState() {
 				this.updateAllGoodsState(!this.isFullCheck)
+			},
+			settlement() {
+				if(!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
+				if(!this.addstr) return uni.$showMsg('请选择收货地址！')
+				if(!this.token) return this.delayNavigate()
+				this.payOrder()
+			},
+			showTips(n) {
+				uni.showToast({
+					icon:'none',
+					title:'请登录后再结算！'+n+'秒后自动跳转到登录页',
+					mask:true,
+					duration:1500
+				})
+			},
+			delayNavigate() {
+				this.second=3
+				this.showTips(this.second)
+				this.timer=setInterval(()=>{
+					this.second--
+					if(this.second<=0) {
+						clearInterval(timer)
+						uni.switchTab({
+							url:'/pages/my/my',
+							success: () => {
+								this.updateRedirectInfo({
+									openType:'switchTab',
+									from: '/pages/cart/cart'
+								})
+							}
+						})
+						return
+					}
+					this.showTips(this.second)
+				},1000)
+			},
+			async payOrder() {
+				const orderInfo={
+					order_price:this.checkedGoodsAmount,
+					consignee_addr:this.addstr,
+					goods:this.cart.filter(x=>x.goods_state).map(x=>({goods_id:x.goods_id,goods_number:x.goods_count,goods_price:x.goods_price}))
+				}
+				const {data:res}=await uni.$http.post('/api/public/v1/my/orders/create',orderInfo)
+				if(res.meta.status!==200) {
+					return uni.$showMsg('创建订单失败！')
+				}
+				const orderNumber=res.message.order_number
+				const {data:res2}=await uni.$http.post('/api/public/v1/my/orders/req_unifiedorder',{order_number:orderNumber})
+				if(res2.meta.status!==200) {
+					return uni.$showMsg('预付订单生成失败！')
+				}
+				const payInfo=res2.message.pay
+				const [err,succ]=await uni.requestPayment(payInfo)
+				if(err) return uni.$showMsg('订单未支付！')
+				const {data:res3}=await uni.$http.post('/api/public/v1/my/orders/chkOrder',{order_number:orderNumber})
+				if(res3.meta.status!==200) {
+					return uni.$showMsg('订单未支付！')
+				}
+				uni.showToast({
+					title:'支付完成！',
+					icon:'success'
+				})
 			}
 		}
 	}
